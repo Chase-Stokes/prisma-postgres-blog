5ccee52df64d216bc50a7776d6f300aef7719b19
chase-stokes <chasehstokes@gmail.com>
2022-10-10 16:11:26 -0700

    debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import AppProps from "next/app"
+import { AppProps } from "next/app"
 import { SessionProvider } from "next-auth/react"
 
 const App = ({ Component, pageProps }: AppProps) => {
--- pages/api/update/[id].ts
+++ pages/api/update/[id].ts
@@ -22,17 +22,3 @@ export default async function handle(req, res) {
     );
   }
 }
-
-// export default async function handle(req, res) {
-//   const postId = req.query.id;
-//   const { title, content } = req.body;
-//   const session = await getSession({ req });
-//   const post = await prisma.post.update({
-//     where: { id: postId },
-//     data: {
-//       title: title,
-//       content: content,
-//     },
-//   });
-//   res.json(post);
-// }
